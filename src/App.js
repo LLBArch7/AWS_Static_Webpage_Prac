86a0821fa2f7699adb92e8c8f8323e103bcbdb92
Lora Lee <107732101+LLBArch7@users.noreply.github.com>
2023-03-24 19:06:38 -0400

    edited for reloading.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          O - kay! I'm reloaded!
         </p>
         <a
           className="App-link"
